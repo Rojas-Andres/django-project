a54fc49e8c89460a1bd1bd1dc4e36c6fdc1d9980
RojasxAndres <afrojaslondono@gmail.com>
2021-03-31 11:59:19 -0500

    Implementando model y migrando a la bd

--- proyecto/proyecto1/models.py
+++ proyecto/proyecto1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Enterprise(models.Model):
+    id = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length = 60,blank=False)
+    nit = models.BigIntegerField(blank = True)
+    gln = models.IntegerField(blank = False)
+
+class Code(models.Model):
+    id = models.IntegerField(primary_key = True)
+    owner = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 60,blank=False)
+    description = models.CharField(max_length = 60,blank = True)
